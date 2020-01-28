26553d4a7d7470e7ff7d8f81c7faacdbacd38625
Patrick <hellohello@hello.com>
2020-01-28 10:30:16 -0500

    P1: Experimented with CSS and started Javascript

--- Exercises/project1/js/script.js
+++ Exercises/project1/js/script.js
@@ -2,10 +2,26 @@
 
 /********************************************************************
 
-Title of Project
-Author Name
+Project 1
+Patrick Chavez-Nadarajah
 
 This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+
+// The script will start once the webpage has loaded
+$(document).ready(setup);
+
+// Adding a variable for slain enemies
+enemiesSlain = 0;
+
+// Adding a variable for the total number of enemies
+enemyTotal;
+
+// script()
+//
+// Activates event handlers that correspond to their respective elements
+function setup() {
+
+}
