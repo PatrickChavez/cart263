76b14ada7460e6ddd1e03df212a6d641ac2e358c
Patrick <hellohello@hello.com>
2020-02-18 17:37:31 -0500

    P2: More experimentation with CSS and starting to script

--- Exercises/project2/js/script.js
+++ Exercises/project2/js/script.js
@@ -9,15 +9,30 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
-
+// Preparing a function that will start once the page has loaded
 $(document).ready(setupDocument);
 
+// Making an array of questions
+let questions = [
+  "Yes for +1P",
+  "No for -1M"
+];
+
+// Activating various functions once the page loads
 function setupDocument() {
 
 }
 
+// Spawning a random dialog box overtime
+function addDialog() {
+  
+}
+
+
+
+
 function setup() {
-createCanvas(900, 500);
+createCanvas(900, 450);
 }
 
 function draw() {
