95c4c91da7d9db94810f515ecc55f3c93209ea84
Patrick <hellohello@hello.com>
2020-03-15 15:08:35 -0400

    P3: Started on the webpage design

--- Exercises/project3/js/script.js
+++ Exercises/project3/js/script.js
@@ -10,9 +10,29 @@ to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
 
-$(document).ready(setup);
+// Preparing a function that will start when the webpage has loaded
+$(document).ready(setupDocument);
 
+// setupDocument()
+//
+// Calls functions once the webpage has loaded
+function setupDocument() {
 
+// $('#textbox').text("Wow!");
+}
+
+// setup()
+//
+// p5 function that sets up other functions at the start of the program
 function setup() {
+  // Creating the canvas and adjusting its position by pixels
+  let p5Canvas = createCanvas(650, 345);
+  p5Canvas.position(355, 115);
+}
 
+// draw()
+//
+// p5 function that calls a function for every frame
+function draw() {
+  background(0);
 }
